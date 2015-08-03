ea30a92aa68d1faebede0fcd0f130a2539921b6e
anuragpapolu <anurag.papolu@gmail.com>
2015-08-03 01:09:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 $("#scroll").click(function() {
-    alert($(".content-section-a").offset().top);
     $('html, body').animate({
         scrollTop: $(".content-section-a").offset().top
     }, 1000);
